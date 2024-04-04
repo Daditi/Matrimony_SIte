e1adc04686d1964ec6ac6357f36e00aa9d26a05f
Aditi Deshmukh <aditideshmukh2000@Aditis-MacBook-Air.local>
2024-04-04 11:24:24 +0530

    Remve Redundant File

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.myapplication.compose.MainScreen
-import com.example.myapplication.ui.theme.MyApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,9 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                MainScreen()
-            }
+            MainScreen()
         }
     }
 }
