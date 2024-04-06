8a7ec2c62952b63b1e6e0469b114312d063f324c
Aditi Deshmukh <aditideshmukh2000@Aditis-MacBook-Air.local>
2024-04-06 12:00:54 +0530

    singleton

--- app/src/main/java/com/example/myapplication/di/AppModule.kt
+++ app/src/main/java/com/example/myapplication/di/AppModule.kt
@@ -9,10 +9,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
     @Provides
     fun providesRetrofit(): Retrofit {
         return Retrofit.Builder()
